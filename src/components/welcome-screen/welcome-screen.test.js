b12d025c1bbc7bee24c1c1428d8aa25a8cadaa88
Илья <bartenev.ilya@gmail.com>
2021-10-02 14:59:51 +0300

    Добавит снепшот тесты

---
+++ src/components/welcome-screen/welcome-screen.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import WelcomeScreen from "./welcome-screen";
+
+it(`RenderWelcomeScreen`, () => {
+  const tree = renderer
+    .create(<WelcomeScreen
+      time={4}
+      errorCount={6}/>)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
