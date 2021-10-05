4bd4dad319a351059fec06cc8144e9acef2e48e7
Илья <bartenev.ilya@gmail.com>
2021-10-05 09:28:42 +0300

    Добавит компонент с вопросом по артистам

---
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {GameType} from "../../const";
+
+const ArtistQuestionScreen = (props) => {
+  const {onAnswer, question} = props;
+  const {answers, song} = question;
+
+  return (
+    <section className="game game--artist">
+      <header className="game__header">
+        <a className="game__back" href="#">
+          <span className="visually-hidden">Сыграть ещё раз</span>
+          <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию"/>
+        </a>
+
+        <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
+          <circle className="timer__line" cx="390" cy="390" r="370"
+            style="filter: url(#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center"/>
+        </svg>
+
+        <div className="timer__value" xmlns="http://www.w3.org/1999/xhtml">
+          <span className="timer__mins">05</span>
+          <span className="timer__dots">:</span>
+          <span className="timer__secs">00</span>
+        </div>
+
+        <div className="game__mistakes">
+          <div className="wrong"></div>
+          <div className="wrong"></div>
+          <div className="wrong"></div>
+        </div>
+      </header>
+
+      <section className="game__screen">
+        <h2 className="game__title">Кто исполняет эту песню?</h2>
+        <div className="game__track">
+          <div className="track">
+            <button className="track__button track__button--play" type="button"></button>
+            <div className="track__status">
+              <audio src={song.src}></audio>
+            </div>
+          </div>
+        </div>
+
+        <form className="game__artist">
+          {answers.map((answer, i) => (
+            <div key={`${answer.artist}-${i}`} className="artist">
+              <input className="artist__input visually-hidden" type="radio" name="answer" value={`artist-${i}`} id={`answer-${i}`}/>
+              <label className="artist__name" htmlFor={`answer-${i}`}>
+                <img className="artist__picture" src="http://placehold.it/134x134" alt="Пелагея"/>
+                    Пелагея
+              </label>
+            </div>
+          )
+          )}
+        </form>
+      </section>
+    </section>
+  );
+};
+
+ArtistQuestionScreen.propTypes = {
+  onAnswer: PropTypes.func.isRequired,
+  question: PropTypes.shape({
+    type: PropTypes.oneOf([GameType.ARTIST, GameType.GENRE]).isRequired,
+    song: PropTypes.shape({
+      artist: PropTypes.string.isRequired,
+      src: PropTypes.string.isRequired,
+    }).isRequired,
+    answers: PropTypes.arrayOf(PropTypes.shape({
+      artist: PropTypes.string.isRequired,
+      picture: PropTypes.string.isRequired,
+    })).isRequired,
+  }).isRequired,
+};
+
+export default ArtistQuestionScreen;
