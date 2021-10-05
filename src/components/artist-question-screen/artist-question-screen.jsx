988d6a8993aa9aa1e2798b2d0a2fe61e01303800
Илья <bartenev.ilya@gmail.com>
2021-10-05 09:41:12 +0300

    Добавит снэпшот тест для компонента ArtistQuestionScreen

--- src/components/artist-question-screen/artist-question-screen.jsx
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -46,7 +46,12 @@ const ArtistQuestionScreen = (props) => {
         <form className="game__artist">
           {answers.map((answer, i) => (
             <div key={`${answer.artist}-${i}`} className="artist">
-              <input className="artist__input visually-hidden" type="radio" name="answer" value={`artist-${i}`} id={`answer-${i}`}/>
+              <input className="artist__input visually-hidden"
+                type="radio" name="answer"
+                value={`artist-${i}`}
+                id={`answer-${i}`}
+                onClick={onAnswer}
+              />
               <label className="artist__name" htmlFor={`answer-${i}`}>
                 <img className="artist__picture" src="http://placehold.it/134x134" alt="Пелагея"/>
                     Пелагея
---
+++ src/components/artist-question-screen/artist-question-screen.test.js
@@ -0,0 +1,37 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import ArtistQuestionScreen from "./artist-question-screen";
+
+const question = {
+  type: `artist`,
+  song: {
+    artist: `Jim Beam`,
+    src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+  },
+  answers: [
+    {
+      picture: `https://lh3.googleusercontent.com/proxy/ELVSk8lMwUNadyXt60N3ckhwjkObTuF7QRCCk5SkhsdUfw7aUO_tFB2XEz99xPS5wePQ9Ey3H6aYGyCAScZbO_1vm6igY_WmCoPkojjNIbdLWOYgbNRPV1xY2h5a5dYL2AWZkt1NM57BkGnBixfw89nTmJSEFsvQvdfgVB-ug4incD1Qe3NUj74`,
+      artist: `John Snow`,
+    },
+    {
+      picture: `https://lh3.googleusercontent.com/proxy/ELVSk8lMwUNadyXt60N3ckhwjkObTuF7QRCCk5SkhsdUfw7aUO_tFB2XEz99xPS5wePQ9Ey3H6aYGyCAScZbO_1vm6igY_WmCoPkojjNIbdLWOYgbNRPV1xY2h5a5dYL2AWZkt1NM57BkGnBixfw89nTmJSEFsvQvdfgVB-ug4incD1Qe3NUj74`,
+      artist: `Jack Daniels`,
+    },
+    {
+      picture: `https://lh3.googleusercontent.com/proxy/ELVSk8lMwUNadyXt60N3ckhwjkObTuF7QRCCk5SkhsdUfw7aUO_tFB2XEz99xPS5wePQ9Ey3H6aYGyCAScZbO_1vm6igY_WmCoPkojjNIbdLWOYgbNRPV1xY2h5a5dYL2AWZkt1NM57BkGnBixfw89nTmJSEFsvQvdfgVB-ug4incD1Qe3NUj74`,
+      artist: `Jim Beam`,
+    },
+  ]
+};
+
+it(`ArtistQuestionScreen is rendered correctly`, () => {
+  const tree = renderer.create(
+      <ArtistQuestionScreen
+        onAnswer={() => {}}
+        question={question}
+      />
+  ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
