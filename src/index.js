0eafaeada9353b3bc746de8980ee7986e8aab63b
Илья <bartenev.ilya@gmail.com>
2021-10-13 12:46:26 +0300

    Подключит redux к проекту

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+import {reducer} from "./reducer";
 import App from "./components/app/app";
 import questions from "./mocks/questions";
 
@@ -9,12 +12,16 @@ const init = () => {
     errorCount: 3,
   };
 
+  const store = createStore(reducer);
+
   ReactDOM.render(
+    <Provider store={store}>
       <App
         errorCount={settings.errorCount}
         gameTime={settings.gameTime}
         questions={questions}
-      />,
+      />
+    </Provider>,
       document.querySelector(`#root`)
   );
 };
